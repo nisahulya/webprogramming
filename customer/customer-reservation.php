db18d89137c8409860c2eebae30eb878c79ba6b7
nisahulya <nisahulya94@gmail.com>
2021-05-13 00:10:40 +0300

    bootstrap added to navbar

--- customer/customer-reservation.php
+++ customer/customer-reservation.php
@@ -1,6 +1,29 @@
 <?php include 'navbar.php'; ?>
 
- <div class="container">
+<head>
+    <style>
+        #info {margin-top: 100px; margin-left: 90px;}
+
+        .button {
+            background-color: #09a3d1;
+            border: none;
+            color: white;
+            padding: 6px 14px;
+            text-align: center;
+            font-size: 16px;
+            margin: 4px 2px;
+            opacity: 0.7;
+            transition: 0.3s;
+            display: inline-block;
+            text-decoration: none;
+            cursor: pointer;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="container">
         <div class="row">
             <div class="col-sm-6">
                 <br>
@@ -47,5 +70,30 @@
             <br>
         </div>
     </div>
+   
+    <script>
+
+        function showInfo(){
+            document.getElementById('info').style.display = "block";
+        }
+
+        function getCheckinDate() {
+        var x = document.getElementById("check-in").value;
+        document.getElementById("check-indate").innerHTML = x;
+        }  
+
+        function getCheckoutDate() {
+        var x = document.getElementById("check-out").value;
+        document.getElementById("check-outdate").innerHTML = x;
+        } 
+
+        function getNumberofPerson() {
+        var x = document.getElementById("numberofguests").value;
+        document.getElementById("numberofperson").innerHTML = x;
+        }
+           
+    </script>
+
+</body>
 
 <?php include 'footer.php'; ?>     
--- customer/navbar.php
+++ customer/navbar.php
@@ -1,6 +1,41 @@
 <!doctype html>
 <html lang="en">
   <head>
+    <style>
+       /* Add a black background color to the top navigation */
+      .topnav {
+        background-color: #17a2b8;
+        overflow: hidden;
+      }
+
+      /* Style the links inside the navigation bar */
+      .topnav a {
+        float: left;
+        display: block;
+        color: #f2f2f2;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+      }
+
+      /* Change the color of links on hover */
+      .topnav a:hover {
+        background-color: #60d9eb;
+
+      }
+
+      /* Add an active class to highlight the current page */
+      .topnav a.active {
+        background-color: #0e6471;
+        color: white;
+      }
+
+      /* Hide the link that should open and close the topnav on small screens */
+      .topnav .icon {
+        display: none;
+      }
+    </style>
     
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -11,35 +46,36 @@
 
     <title>Hotel Software</title>
   </head>
+ 
   <body>
-    <nav class="navbar navbar-expand-sm bg-info navbar-dark d-flex justify-content-between">
-      <div class="d-flex align-items-center">
+    <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-          <!-- Links -->
-          <ul class="navbar-nav" style="margin: auto;">
-              <!-- Hoetl logo -->
-              <a class="navbar-brand" href="home.php" target="_self">
-                  <img src="../docs/images/logo.png" alt="logo" style="width:70px;">
-              </a>
-              <li class="nav-item active p-3 ">
-                  <a class="nav-link" href="<?php $link="about-hotel.php"; echo $link ?>" >About Hotel</a>
-              </li>
-              <li class="nav-item active p-3">
-                <a class="nav-link" href="customer-reservation.php">New Reservation</a>
-              </li>
-              <li class="nav-item active p-3">
-                  <a class="nav-link" href="customer-reservation-update.php">My Reservations</a>
-              </li>
+    <div class="topnav" id="myTopnav">
+      <a href="#home" >
+        <img src="../docs/images/logo.png" alt="logo" style="width:35px; height:25px;">
+      </a>
+      <a href="about-hotel.php" >About Hotel</a>
+      <a href="customer-reservation.php">New Reservation</a>
+      <a href="#contact">My Reservations</a>
+      <a style="float:right" href="#about">Logout</a>
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+      </a>
+    </div>
 
-          </ul>
-      </div>
+    <script>
+      /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+      function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topnav";
+        }
+      }
+    </script>
 
-      <div>
-          
-          <ul class="navbar-nav">
-              <li class="nav-item active ">
-                  <a class="nav-link" href="#">Logout</a>
-              </li>
-          </ul>
-      </div>
-    </nav>
+  </body>
+
+</html>
